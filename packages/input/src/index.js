b120b8ab76de59f111f4a74d027cbc835786bc92
appear <appear.ko@gmail.com>
2020-07-11 20:20:11 +0900

    setup monorepo

---
+++ packages/input/src/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+import Button from "../../button/src";
+
+console.log(Button);
+
+const Input = styled.input`
+  background: red;
+  color: #fff;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: 300;
+  padding: 9px 36px;
+`;
+
+export default Input;
